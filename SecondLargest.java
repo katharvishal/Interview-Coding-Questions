5da00a391a6c0bd775e2ae000ea474138df7d398
vishal Kathar <katharvishal73@gmail.com>
2026-09-11 12:54:12 +0530

    Refactor SecondLargest class to empty main method
    Removed logic for finding the second largest element.

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,23 +1,5 @@
 public class SecondLargest {
     public static void main(String[] args) {
 
-        int[] arr = {10,5,20,8,5};
-        int max = arr[0];
-        int secondMax = Integer.MIN_VALUE;
-
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                secondMax = max;
-                max = arr[i];
-            } else if(arr[i] > secondMax && arr[i] != max){
-                secondMax = arr[i];
-            }
-        }
-
-        if(secondMax == Integer.MIN_VALUE){
-            System.out.println("There is no second largest element.");
-        } else {
-            System.out.println("The second largest element is: " + secondMax);
-        } 
     }
 }
