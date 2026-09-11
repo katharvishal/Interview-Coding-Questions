a0f6137d2364b089c23b272423d749546c8ac925
vishal Kathar <katharvishal73@gmail.com>
2026-09-11 12:59:09 +0530

    Remove second largest element logic from SecondLargest
    Removed logic for finding the second largest element.

--- SecondLargest.java
+++ SecondLargest.java
@@ -5,20 +5,7 @@ public class SecondLargest {
         int max = arr[0];
         int secondMax = Integer.MIN_VALUE;
 
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                secondMax = max;
-                max = arr[i];
-            } else if(arr[i] > secondMax && arr[i] != max){
-                secondMax = arr[i];
-            }
-        }
-
-        if(secondMax == Integer.MIN_VALUE){
-            System.out.println("There is no second largest element.");
-        } else {
-            System.out.println("The second largest element is: " + secondMax);
-        }
+        
         
     }
 }
